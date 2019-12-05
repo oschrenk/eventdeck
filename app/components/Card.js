3c0001f8ce60f4daf2e11ba4dbcc55539efc7dc4
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-05 14:34:54 +0100

    Add City event card image assets

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ImageBackground } from 'react-native';
 
 export default function Card(props) {
   if (props.side === 'front')
     return (
-      <View>
-        <Text>{props.content.front.text}</Text>
-        <Text>{props.content.front.optionA}</Text>
-        <Text>{props.content.front.optionB}</Text>
+      <View style={{width: 378, height: 530}}>
+        <ImageBackground source={require('../../assets/images/city_front.png')} style={{width: '100%', height: '100%'}}>
+          <Text>{props.content.front.text}</Text>
+          <Text>{props.content.front.optionA}</Text>
+          <Text>{props.content.front.optionB}</Text>
+        </ImageBackground>
       </View>
     );
   else
     return (
-      <View>
-        <Text>{props.content.back.optionA}</Text>
-        <Text>{props.content.back.optionB}</Text>
+      <View style={{width: 378, height: 530}}>
+        <ImageBackground source={require('../../assets/images/city_back.png')} style={{width: '100%', height: '100%'}}>
+          <Text>{props.content.back.optionA}</Text>
+          <Text>{props.content.back.optionB}</Text>
+        </ImageBackground>
       </View>
     );
 
