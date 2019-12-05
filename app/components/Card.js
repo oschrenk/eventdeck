373d7472a34d3164ef00e36cbc43ff59f64c3627
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-05 15:03:16 +0100

    Style card text

--- app/components/Card.js
+++ app/components/Card.js
@@ -6,9 +6,9 @@ export default function Card(props) {
     return (
       <View style={{width: 378, height: 530}}>
         <ImageBackground source={require('../../assets/images/city_front.png')} style={{width: '100%', height: '100%'}}>
-          <Text style={{left: 40, top: 100, color: 'white'}}>{props.content.front.text}</Text>
-          <Text style={{left: 40, top: 400, color: 'white'}}>Option A: {props.content.front.optionA}</Text>
-          <Text style={{left: 40, top: 420, color: 'white'}}>Option B: {props.content.front.optionB}</Text>
+          <Text style={{left: 40, top: 100, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.content.front.text}</Text>
+          <Text style={{left: 40, top: 400, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>Option A: {props.content.front.optionA}</Text>
+          <Text style={{left: 40, top: 420, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>Option B: {props.content.front.optionB}</Text>
         </ImageBackground>
       </View>
     );
@@ -16,8 +16,8 @@ export default function Card(props) {
     return (
       <View style={{width: 378, height: 530}}>
         <ImageBackground source={require('../../assets/images/city_back.png')} style={{width: '100%', height: '100%'}}>
-          <Text style={{left: 60, top: 20, color: 'white'}}>{props.content.back.optionA}</Text>
-          <Text style={{left: 60, top: 280, color: 'white'}}>{props.content.back.optionB}</Text>
+          <Text style={{left: 60, top: 20, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.content.back.optionA}</Text>
+          <Text style={{left: 60, top: 280, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.content.back.optionB}</Text>
         </ImageBackground>
       </View>
     );
