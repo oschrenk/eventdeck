f6057f0be1dcc0c2c2e92d5bb6af661a798d8bcb
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-06 18:08:01 +0100

    Hoist flipCard

--- App.js
+++ App.js
@@ -17,17 +17,20 @@ const card = {
 
 const App: () => React$Node = () => {
   const [side, setSide] = useState('front');
+  const [currentCard, setCard] = useState(card1)
+  const flipCard = () => {
+    if(side === 'front')
+      setSide('back');
+    else
+      setSide('front');
+  }
+
   return (
     <>
       <ImageBackground source={require('./assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
         <SafeAreaView>
-          <Card card={card} side={side} />
-          <Button title="Flip" onPress={() => {
-            if(side === 'front')
-              setSide('back');
-            else
-              setSide('front');
-          }} />
+          <Card card={currentCard} side={side} />
+          <Button title="Flip" onPress={flipCard} />
         </SafeAreaView>
       </ImageBackground>
     </>
