ee0bef9ad34d26fb2d98a9dadc8d5197ed996c09
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-06 18:14:52 +0100

    Simulate drawing cards by toggling between two

--- App.js
+++ App.js
@@ -3,15 +3,29 @@ import {SafeAreaView, Button, ImageBackground} from 'react-native';
 
 import Card from './app/components/Card';
 
-const card = {
-  text: "Flavor",
+const card1 = {
+  id: "1",
+  text: "Card 1",
   optionA: {
-    choice: "Be nice",
-    outcome: [{text: "Be lucky"}]
+    choice: "1 Be nice",
+    outcome: [{text: "1 Be lucky"}]
   },
   optionB: {
-    choice: "Be naughty",
-    outcome: [{text: "Be punished"}]
+    choice: "1 Be naughty",
+    outcome: [{text: "1 Be punished"}]
+  }
+}
+
+const card2 = {
+  id: "2",
+  text: "Card 2",
+  optionA: {
+    choice: "2 Be nice",
+    outcome: [{text: "2 Be lucky"}]
+  },
+  optionB: {
+    choice: "2 Be naughty",
+    outcome: [{text: "2 Be punished"}]
   }
 }
 
@@ -24,11 +38,25 @@ const App: () => React$Node = () => {
     else
       setSide('front');
   }
+  const drawCard = () => {
+
+    console.log("currentCard", currentCard)
+    if(currentCard.id === "1") {
+      console.log("if", currentCard)
+      setCard(card2)
+      setSide('front')
+    } else {
+      console.log("else", currentCard)
+      setCard(card1)
+      setSide('front')
+    }
+  }
 
   return (
     <>
       <ImageBackground source={require('./assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
         <SafeAreaView>
+          <Button title="Draw" onPress={drawCard} />
           <Card card={currentCard} side={side} />
           <Button title="Flip" onPress={flipCard} />
         </SafeAreaView>
