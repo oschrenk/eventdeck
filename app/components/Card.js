3c1e2a81b00eb658595ca86a26b3566004886f7a
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-07 13:20:09 +0100

    Better styling

--- app/components/Card.js
+++ app/components/Card.js
@@ -6,9 +6,20 @@ export default function Card(props) {
     return (
       <View style={{width: 378, height: 530}}>
         <ImageBackground source={require('../../assets/images/city_front.png')} style={{width: '100%', height: '100%'}}>
-          <Text style={{left: 40, top: 100, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.card.text}</Text>
-          <Text style={{left: 40, top: 400, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>Option A: {props.card.optionA.choice}</Text>
-          <Text style={{left: 40, top: 420, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>Option B: {props.card.optionB.choice}</Text>
+          <View style={{left: 40, top: 80, width: 305, height: "80%"}}>
+            <Text style={{color: 'white', fontFamily: 'Nyala', fontSize: 16 }}>{props.card.text}</Text>
+          </View>
+          <View style={{left: 40, width: 305, height: "16%"}}>
+            <Text style={{color: 'white', fontFamily: 'Nyala', fontSize: 16 }}>
+              <Text style={{fontWeight: "bold"}}>Option A:</Text> {props.card.optionA.choice}
+            </Text>
+            <Text style={{top: 12, color: 'white', fontFamily: 'Nyala', fontSize: 16 }}>
+              <Text style={{fontWeight: "bold"}}>Option B:</Text> {props.card.optionB.choice}
+            </Text>
+          </View>
+          <View style={{height: "4%", width: "100%"}}>
+              <Text style={{color: 'white', fontFamily: 'Nyala', fontSize: 16, textAlign: 'center'}}>{props.card.id}</Text>
+          </View>
         </ImageBackground>
       </View>
     );
@@ -16,8 +27,12 @@ export default function Card(props) {
     return (
       <View style={{width: 378, height: 530}}>
         <ImageBackground source={require('../../assets/images/city_back.png')} style={{width: '100%', height: '100%'}}>
-          <Text style={{left: 60, top: 20, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.card.optionA.outcome[0].text}</Text>
-          <Text style={{left: 60, top: 280, color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.card.optionB.outcome[0].text}</Text>
+          <View style={{left: 60, top: 20, width: 305, height: "50%"}}>
+            <Text style={{color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.card.optionA.outcome[0].text}</Text>
+          </View>
+          <View style={{left: 60, top: 30, width: 305, height: "50%"}}>
+            <Text style={{color: 'white', fontFamily: 'Nyala', fontSize: 18 }}>{props.card.optionB.outcome[0].text}</Text>
+          </View>
         </ImageBackground>
       </View>
     );
