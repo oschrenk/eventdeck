516ba3e5288fb5954866b19679f19c3c52b3abc2
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-08 15:03:06 +0100

    Extract HomeScreen

--- App.js
+++ App.js
@@ -1,45 +1,10 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Button, ImageBackground} from 'react-native';
-
-import Card from './app/components/Card';
-import defaultData from './app/data/events.json'
-
-const cards = defaultData
+import React from 'react';
+import HomeScreen from './app/screens/HomeScreen'
 
 const App: () => React$Node = () => {
-  const [side, setSide] = useState('front');
-  const [currentCard, setCard] = useState(null)
-  const flipCard = () => {
-    if(side === 'front')
-      setSide('back');
-    else
-      setSide('front');
-  }
-  const drawCard = () => {
-    const newCardId = Math.floor((Math.random() * cards.length) + 1);
-    const newCard = cards.find(c => c.id === newCardId)
-    setCard(newCard)
-    setSide('front')
-  }
-  const putBack = () => {
-    setCard(null)
-    setSide('front')
-  }
-
   return (
     <>
-      <ImageBackground source={require('./assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
-        <SafeAreaView>
-          <Button title="Draw" onPress={drawCard} />
-          { currentCard &&
-            <>
-              <Button title="Put back" onPress={putBack} />
-              <Card card={currentCard} side={side} />
-              <Button title="Flip" onPress={flipCard} />
-            </>
-          }
-        </SafeAreaView>
-      </ImageBackground>
+      <HomeScreen />
     </>
   );
 };
