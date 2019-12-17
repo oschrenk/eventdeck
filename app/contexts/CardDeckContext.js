5e032fe3fbd3974ee1eb14d151eb5d4f6d46e852
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-17 15:59:46 +0100

    Move city events to city.json

--- app/contexts/CardDeckContext.js
+++ app/contexts/CardDeckContext.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 
-import defaultData from '../data/events.json'
+import defaultData from '../data/city.json'
 
 const CardDeckContext = React.createContext([{}, () => {}]);
 
