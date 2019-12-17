7d915782133f86ed4effe8955cce0f500ed86689
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-17 17:37:45 +0100

    Add road event type

--- app/components/Card.js
+++ app/components/Card.js
@@ -27,9 +27,15 @@ const view = {
   Return: "<Icon name={'return-to-deck'} style={{fontSize: 32, textAlign: 'right' }}/>",
 }
 
-const city = {
-  back: require('../../assets/images/city_back.png'),
-  front: require('../../assets/images/city_front.png')
+const backgrounds = {
+  city: {
+    back: require('../../assets/images/city_back.png'),
+    front: require('../../assets/images/city_front.png')
+  },
+  road: {
+    back: require('../../assets/images/road_back.png'),
+    front: require('../../assets/images/road_front.png')
+  }
 }
 
 const Bold = ({text}) => {
@@ -41,46 +47,52 @@ const enhance = (text) => {
   return Mustache.render(text, view)
 }
 
-
 const Front = (props) => {
+  const color = colors[props.card['type']]
   return (
     <View style={{width: 378, height: 530}}>
-      <ImageBackground source={city.front} style={{width: '100%', height: '100%'}}>
+      <ImageBackground source={backgrounds[props.card['type']]['front']} style={{width: '100%', height: '100%'}}>
         <View style={{left: 40, top: 80, width: 305, height: "80%"}}>
-          <Text style={frontText}>{props.card.text}</Text>
+          <Text style={{...frontText, color: color}}>{props.card.text}</Text>
         </View>
         <View style={{left: 40, width: 305, height: "16%"}}>
-          <Text style={frontText}>
+          <Text style={{...frontText, color: color}}>
             <Bold text={'Option A'} /> {props.card.optionA.choice}
           </Text>
-          <Text style={{...frontText, top: 12}}>
+          <Text style={{...frontText, color: color, top: 12}}>
             <Bold text={'Option B'} /> {props.card.optionB.choice}
           </Text>
         </View>
         <View style={{height: "4%", width: "100%"}}>
-          <Text style={{...frontText, textAlign: 'center'}}>{props.card.id}</Text>
+          <Text style={{...frontText, color: color, textAlign: 'center'}}>{props.card.id}</Text>
         </View>
       </ImageBackground>
     </View>
   );
 }
 
-const Outcome = ({outcome}) => {
+const colors = {
+  city: 'white',
+  road: 'black'
+}
+
+const Outcome = ({outcome, type}) => {
   const requirement = outcome.requirement ? `${outcome.requirement}: ` : ""
   const text = outcome.text ? `${outcome.text}` : ""
   const effects = outcome.effects ? `${outcome.effects.join("\n")}` : ""
   const all = `${requirement}${text}\n${effects}`
 
+  const color = colors[type]
   return (
     <View>
-      <Text style={backText}>
+      <Text style={{...backText, color: color}}>
         <JsxParser
           components={{ Icon, Text }}
           jsx={enhance(all)}
           renderInWrapper={false}
         />
       </Text>
-      <Text style={backText}>
+      <Text style={{...backText, color: color}}>
         <JsxParser
           components={{ Icon, Text }}
           jsx={enhance(outcome.resolve)}
@@ -92,19 +104,19 @@ const Outcome = ({outcome}) => {
   )
 }
 
-const Outcomes = ({outcomes}) => {
-  return outcomes.map((outcome, index) => <Outcome outcome={outcome} key={index} />)
+const Outcomes = ({outcomes, type}) => {
+  return outcomes.map((outcome, index) => <Outcome outcome={outcome} type={type} key={index} />)
 }
 
 const Back = (props) => {
   return (
     <View style={{width: 378, height: 530}}>
-      <ImageBackground source={city.back} style={{width: '100%', height: '100%'}}>
+      <ImageBackground source={backgrounds[props.card['type']]['back']} style={{width: '100%', height: '100%'}}>
         <View style={{left: 60, top: 20, width: 280, height: "50%"}}>
-          <Outcomes outcomes={props.card.optionA.outcomes} />
+          <Outcomes outcomes={props.card.optionA.outcomes} type={props.card['type']} />
         </View>
         <View style={{left: 60, top: 25, width: 280, height: "50%"}}>
-          <Outcomes outcomes={props.card.optionB.outcomes} />
+          <Outcomes outcomes={props.card.optionB.outcomes} type={props.card['type']} />
         </View>
       </ImageBackground>
     </View>
--- app/screens/EditScreen.js
+++ app/screens/EditScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import {ImageBackground, FlatList, SafeAreaView} from 'react-native';
 import Checkbox from 'react-native-modest-checkbox'
 
-import { useCardDeck, allCardNumbers } from "../contexts/CardDeckContext";
+import { useCardDeck, allCityCardNumbers } from "../contexts/CardDeckContext";
 import { frontText } from '../styles/typography'
 
 const EditScreen = ({navigation}) => {
@@ -12,13 +12,13 @@ const EditScreen = ({navigation}) => {
     <ImageBackground source={require('../../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
       <SafeAreaView>
         <FlatList
-          data={allCardNumbers}
+          data={allCityCardNumbers}
           renderItem={({ item }) =>
             <Checkbox
               label={item.toString()}
-              checked={available.has(item)}
+              checked={available.city.has(item)}
               labelStyle={frontText}
-              onChange={(item) => toggleAvailable(item)}
+              onChange={(item) => toggleAvailable(item, 'city')}
             />
           }
           keyExtractor={item => item.toString()}
