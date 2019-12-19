bc2bd265c34b7117eb913b78d081ce44f5cf3800
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-19 17:36:19 +0100

    Prepare for transforming road events

--- prepare.sh
+++ prepare.sh
@@ -6,10 +6,12 @@ TARGET="app/data/city.json"
 
 curl https://raw.githubusercontent.com/oschrenk/gloomhavendb/city-events-1-to-30/src/data/events.ts -o $SOURCE
 
+# prepare city events
+TYPE="city"
 echo "" > $STUB
 echo -e "[\n" >> $STUB
 tail -n +5 $SOURCE | head -631 | grep -v EventType >> $STUB
 echo -e "\n]" >> $STUB
 
-node transform.js $STUB $TARGET
+node transform.js "city" $STUB $TARGET
 
