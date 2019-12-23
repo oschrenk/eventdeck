b2e309ba10934257ac1dbe34f2331de56e0e9414
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-23 12:33:51 +0100

    Add Party Screen

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationNativeContainer } from '@react-navigation/native';
 
-import HomeScreen from './app/screens/HomeScreen'
 import EditScreen from './app/screens/EditScreen'
+import HomeScreen from './app/screens/HomeScreen'
+import PartyScreen from './app/screens/PartyScreen'
 import SettingsScreen from './app/screens/SettingsScreen'
 
 import { CardDeckProvider } from './app/contexts/CardDeckContext'
@@ -21,6 +22,7 @@ const App = () => {
         <Drawer.Navigator initialRouteName="Home">
           <Drawer.Screen name="Home" component={HomeScreen} />
           <Drawer.Screen name="Edit" component={EditScreen} />
+          <Drawer.Screen name="Party" component={PartyScreen} />
           <Drawer.Screen name="Settings" component={SettingsScreen} />
         </Drawer.Navigator>
       </NavigationNativeContainer>
