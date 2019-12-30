b0249ea85afce1e9bb5f27e17ef90bfb838b2b7f
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-12-30 13:42:30 +0100

    Show current party in Home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {ImageBackground, SafeAreaView, View} from 'react-native';
+import {ImageBackground, SafeAreaView, Text, View} from 'react-native';
 
 import Card from '../components/Card';
 import { useCardDeck } from "../contexts/CardDeckContext";
 import IconButton from '../components/IconButton'
 import ImageButton from '../components/ImageButton'
+import { partyName } from '../styles/typography'
 
 const HomeScreen = () => {
-  const { currentCard, side, drawCard, flipCard, putBack, destroy } = useCardDeck()
+  const { currentCard, currentParty, side, drawCard, flipCard, putBack, destroy } = useCardDeck()
 
   return (
     <ImageBackground source={require('../../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
       <SafeAreaView>
+        <Text style={{...partyName, textAlign: 'center'}}>{currentParty().name}</Text>
         <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', height: 48}}>
           <ImageButton source={require('../../assets/images/city_button.png')} onPress={() => drawCard('city')} style={{ height: 40, resizeMode: 'center'}}/>
           <ImageButton source={require('../../assets/images/road_button.png')} onPress={() => drawCard('road')} style={{ height: 40, resizeMode: 'center'}}/>
