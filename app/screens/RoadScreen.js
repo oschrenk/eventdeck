0290d9105ebf7b22e746c1cb5f3aa081520380e9
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-01-04 16:44:12 +0100

    Split Edit screens into City/Road

--- app/screens/RoadScreen.js
+++ app/screens/RoadScreen.js
@@ -5,7 +5,7 @@ import Checkbox from 'react-native-modest-checkbox'
 import { header } from '../styles/typography'
 
 
-import { useCardDeck, allCityCardNumbers, allRoadCardNumbers } from "../contexts/CardDeckContext";
+import { useCardDeck, allRoadCardNumbers } from "../contexts/CardDeckContext";
 import { frontText } from '../styles/typography'
 
 const Column = ({ids, checked, toggle}) => {
@@ -27,30 +27,12 @@ const Column = ({ids, checked, toggle}) => {
   )
 }
 
-const EditScreen = ({navigation}) => {
+const RoadScreen = ({navigation}) => {
   const { isAvailable, toggleAvailable } = useCardDeck()
 
   return (
     <ImageBackground source={require('../../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
       <SafeAreaView>
-        <Text style={header}>City, Base</Text>
-        <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
-            <Column ids={allCityCardNumbers.slice(0,6)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(6,12)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(12,18)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(18,24)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(24,30)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-        </View>
-
-        <Text style={header}>City, Expanded</Text>
-        <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
-            <Column ids={allCityCardNumbers.slice(30,35)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(35,40)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(40,45)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(0,0)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-            <Column ids={allCityCardNumbers.slice(0,0)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-        </View>
-
         <Text style={header}>Road, Base</Text>
         <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
             <Column ids={allRoadCardNumbers.slice(0,6)} checked={(item) => isAvailable(item, 'road')} toggle={(item) => toggleAvailable(item, 'road')}/>
@@ -73,4 +55,4 @@ const EditScreen = ({navigation}) => {
   );
 };
 
-export default EditScreen;
+export default RoadScreen;
