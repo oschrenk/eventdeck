63349671fc045bae264f271f3666b20a89d04869
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-01-17 00:50:25 +0100

    Make fonts smaller

--- app/styles/typography.js
+++ app/styles/typography.js
@@ -1,49 +1,49 @@
 export const header = {
   fontFamily: 'Pirata One',
-  fontSize: 32,
+  fontSize: 30,
   color: 'black'
 };
 
 export const partyName = {
   fontFamily: 'Pirata One',
-  fontSize: 22,
+  fontSize: 20,
   color: 'black'
 }
 
 export const input = {
   fontFamily: 'Pirata One',
-  fontSize: 24,
+  fontSize: 20,
   color: 'black'
 }
 
 export const backText = {
   fontFamily: 'HighTowerText-Italic',
-  fontSize: 15,
+  fontSize: 14,
 };
 
 export const backTextInstruction = {
   fontFamily: 'HighTowerText-Reg',
-  fontSize: 15,
+  fontSize: 14,
 };
 
 export const backTextEffect = {
   fontFamily: 'HighTowerText-Bold',
-  fontSize: 16,
+  fontSize: 15,
 };
 
 export const frontText = {
   fontFamily: 'HighTowerText-Italic',
-  fontSize: 15,
+  fontSize: 14,
 };
 
 export const bold = {
   fontFamily: 'HighTowerText-Bold',
   fontWeight: 'bold',
-  fontSize: 16,
+  fontSize: 14,
 };
 
 export const icon = {
   fontFamily: 'icomoon',
-  fontSize: 16
+  fontSize: 14
 };
 
