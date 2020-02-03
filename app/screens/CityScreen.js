0e90ceb064e9b8ccc5fea7276f29df54830f65a9
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-03 14:38:26 +0100

    City 89

--- app/screens/CityScreen.js
+++ app/screens/CityScreen.js
@@ -63,9 +63,9 @@ const CityScreen = ({navigation}) => {
           <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
               <Column ids={allCityCardNumbers.slice(81,82)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
               <Column ids={allCityCardNumbers.slice(82,83)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(83,84)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(84,86)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(86,88)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+              <Column ids={allCityCardNumbers.slice(83,85)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+              <Column ids={allCityCardNumbers.slice(85,87)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+              <Column ids={allCityCardNumbers.slice(87,89)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
           </View>
         </ScrollView>
       </SafeAreaView>
