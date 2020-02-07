aac69447439400f52410cb65ad15cfa2123edf25
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-07 17:42:48 +0100

    Externalize EnrichedText component

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,50 +1,10 @@
 import React from 'react';
 import { Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 
-import { backText, backTextEffect,backTextInstruction, frontText, bold } from '../styles/typography'
-import Icon, { CardIcon, StyledIcon } from './Icon'
+import { backText, backTextEffect, backTextInstruction, frontText, bold } from '../styles/typography'
+import { CardIcon } from './Icon'
+import EnrichedText from './EnrichedText'
 import { useCardDeck } from "../contexts/CardDeckContext"
-
-const Lookup = {
-  //classes
-  "Brute": <Icon name={'brute'} key={'brute'}/>,
-  "Circles": <Icon name={'circles'} key={'circles'}/>,
-  "Cragheart": <Icon name={'cragheart'} key={'cragheart'}/>,
-  "Cthulu": <Icon name={'cthulu'} key={'cthulu'}/>,
-  "Eclipse": <Icon name={'eclipse'} key={'eclipse'}/>,
-  "LightningBolts": <Icon name={'eclipse'} key={'eclipse'}/>,
-  "Mindthief": <Icon name={'mindthief'} key={'mindthief'}/>,
-  "MusicNote": <Icon name={'music-note'} key={'music-note'}/>,
-  "PointyHead": <Icon name={'pointy-head'} key={'pointy-head'}/>,
-  "Saw": <Icon name={'saw'} key={'saw'}/>,
-  "Scoundrel": <Icon name={'scoundrel'} key={'scoundrel'}/>,
-  "Spellweaver": <Icon name={'spellweaver'} key={'spellweaver'}/>,
-  "Sun": <Icon name={'sun'} key={'sun'}/>,
-  "ThreeSpears": <Icon name={'three-spears'} key={'three-spears'}/>,
-  "Tinkerer": <Icon name={'tinkerer'} key={'tinkerer'}/>,
-  "Triangles": <Icon name={'triangles'} key={'triangles'}/>,
-  "TwoMinis": <Icon name={'two-minis'} key={'key-minis'}/>,
-
-  // Status
-  'Bless': <Icon name={'bless'} key={'bless'}/>,
-  "Check": <Icon name={'check'} key={'check'}/>,
-  'Curse': <Icon name={'curse'} key={'curse'}/>,
-  'Disarm': <Icon name={'disarm'} key={'disarm'}/>,
-  'Immobilize': <Icon name={'immobilize'} key={'immobilize'}/>,
-  'Muddle': <Icon name={'muddle'} key={'muddle'}/>,
-  'Poison': <Icon name={'poison'} key={'poison'}/>,
-  'Strengthen': <Icon name={'strengthen'} key={'strengthen'}/>,
-  'Wound': <Icon name={'wound'} key={'wound'}/>,
-
-  // Other
-  'MinusOneAttackModifier': <Icon name={'1-attack-modifier'} key={'1-attack-modifier'}/>,
-  'PlusOneEnhancement': <Icon name={'plus-one-enhancement'} key={'plus-one-enhancement'}/>,
-  'SmallItem': <Icon name={'small-item'} key={'small-item'}/>,
-  'Checkbox': <Icon name={'checkbox'} key={'checkbox'}/>,
-
-  // Resolves
-  'Remove': <StyledIcon name={'remove-from-game'} style={{fontSize: 18, color: 'white' }} key={'remove-from-game2'}/>,
-}
 
 const backgrounds = {
   city: {
@@ -154,7 +114,7 @@ const RequirementBlock = ({requirement, text, color}) => {
   // back and not view, so we can nest them in another Text element
   return (
     <Text>
-      <TextParser text={requirement + ": " + text} style={{color:color}} />
+      <EnrichedText text={requirement + ": " + text} style={{color:color}} />
     </Text>
   )
 }
@@ -163,7 +123,7 @@ const InstructionBlock = ({text, color}) => {
   // same text parsing logic and caveats as requirements applies
   return (
     <Text>
-      <TextParser text={text} style={{...backTextInstruction, color:color, lineHeight: 15}} />
+      <EnrichedText text={text} style={{...backTextInstruction, color:color, lineHeight: 15}} />
     </Text>
   )
 }
@@ -197,54 +157,13 @@ const Blocks = ({instruction, requirement, texts, color }) => {
   )
 }
 
-const KeywordRegex = /\s*(\{\w+\})\s*/g;
-const splitKeywords = (text) => {
-  return text.split(KeywordRegex).filter(s => s !== "")
-}
-
-const TextParser = ({text, style}) => {
-  if (!text) {
-    return null
-  }
-  const split = splitKeywords(text)
-  return (
-    split.map((s,i) => {
-      if (s.startsWith("{")) {
-        const name = s.substr(1, s.length - 2)
-        const lookup = Lookup[name]
-        if (name.startsWith("Scenario")) {
-          const numberPattern = /\d+/g
-          const match = name.match(numberPattern).map(Number)
-          const number = parseInt(match.join([]))
-          const iconName = 'scenario-' + number
-          return <Icon name={iconName} key={i}/>
-        } else if (name.startsWith("Rune")) {
-          const rune = name.slice(-1)
-          const iconName = 'rune-' + rune
-          return <Icon name={iconName} key={i}/>
-        } else if (!lookup) {
-          console.warn("MISSING", name)
-          return null
-        } else if (name.startsWith("Remove")) {
-          return lookup
-        } else {
-          return lookup
-        }
-      } else {
-        return <Text style={{...backText, ...style}} key={i}>{s}</Text>
-      }
-    })
-  )
-}
-
 const Effect = ({text, color}) => {
   return (
     <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-      <TextParser text={text} style={{...backTextEffect, color:color}}/>
+      <EnrichedText text={text} style={{...backTextEffect, color:color}}/>
     </View>
   )
 }
-
 
 const Resolve = ({option, resolve, effects}) => {
   if (!resolve) {
---
+++ app/components/EnrichedText.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import Icon, { StyledIcon } from './Icon'
+
+import { backText } from '../styles/typography'
+
+const Lookup = {
+  //classes
+  "Brute": <Icon name={'brute'} key={'brute'}/>,
+  "Circles": <Icon name={'circles'} key={'circles'}/>,
+  "Cragheart": <Icon name={'cragheart'} key={'cragheart'}/>,
+  "Cthulu": <Icon name={'cthulu'} key={'cthulu'}/>,
+  "Eclipse": <Icon name={'eclipse'} key={'eclipse'}/>,
+  "LightningBolts": <Icon name={'eclipse'} key={'eclipse'}/>,
+  "Mindthief": <Icon name={'mindthief'} key={'mindthief'}/>,
+  "MusicNote": <Icon name={'music-note'} key={'music-note'}/>,
+  "PointyHead": <Icon name={'pointy-head'} key={'pointy-head'}/>,
+  "Saw": <Icon name={'saw'} key={'saw'}/>,
+  "Scoundrel": <Icon name={'scoundrel'} key={'scoundrel'}/>,
+  "Spellweaver": <Icon name={'spellweaver'} key={'spellweaver'}/>,
+  "Sun": <Icon name={'sun'} key={'sun'}/>,
+  "ThreeSpears": <Icon name={'three-spears'} key={'three-spears'}/>,
+  "Tinkerer": <Icon name={'tinkerer'} key={'tinkerer'}/>,
+  "Triangles": <Icon name={'triangles'} key={'triangles'}/>,
+  "TwoMinis": <Icon name={'two-minis'} key={'key-minis'}/>,
+
+  // Status
+  'Bless': <Icon name={'bless'} key={'bless'}/>,
+  "Check": <Icon name={'check'} key={'check'}/>,
+  'Curse': <Icon name={'curse'} key={'curse'}/>,
+  'Disarm': <Icon name={'disarm'} key={'disarm'}/>,
+  'Immobilize': <Icon name={'immobilize'} key={'immobilize'}/>,
+  'Muddle': <Icon name={'muddle'} key={'muddle'}/>,
+  'Poison': <Icon name={'poison'} key={'poison'}/>,
+  'Strengthen': <Icon name={'strengthen'} key={'strengthen'}/>,
+  'Wound': <Icon name={'wound'} key={'wound'}/>,
+
+  // Other
+  'MinusOneAttackModifier': <Icon name={'1-attack-modifier'} key={'1-attack-modifier'}/>,
+  'PlusOneEnhancement': <Icon name={'plus-one-enhancement'} key={'plus-one-enhancement'}/>,
+  'SmallItem': <Icon name={'small-item'} key={'small-item'}/>,
+  'Checkbox': <Icon name={'checkbox'} key={'checkbox'}/>,
+
+  // Resolves
+  'Remove': <StyledIcon name={'remove-from-game'} style={{fontSize: 18, color: 'white' }} key={'remove-from-game2'}/>,
+}
+
+const KeywordRegex = /\s*(\{\w+\})\s*/g;
+const splitKeywords = (text) => {
+  return text.split(KeywordRegex).filter(s => s !== "")
+}
+
+export default EnrichedText = ({text, style}) => {
+  if (!text) {
+    return null
+  }
+  const split = splitKeywords(text)
+  return (
+    split.map((s,i) => {
+      if (s.startsWith("{")) {
+        const name = s.substr(1, s.length - 2)
+        const lookup = Lookup[name]
+        if (name.startsWith("Scenario")) {
+          const numberPattern = /\d+/g
+          const match = name.match(numberPattern).map(Number)
+          const number = parseInt(match.join([]))
+          const iconName = 'scenario-' + number
+          return <Icon name={iconName} key={i}/>
+        } else if (name.startsWith("Rune")) {
+          const rune = name.slice(-1)
+          const iconName = 'rune-' + rune
+          return <Icon name={iconName} key={i}/>
+        } else if (!lookup) {
+          console.warn("MISSING", name)
+          return null
+        } else if (name.startsWith("Remove")) {
+          return lookup
+        } else {
+          return lookup
+        }
+      } else {
+        return <Text style={{...backText, ...style}} key={i}>{s}</Text>
+      }
+    })
+  )
+}
