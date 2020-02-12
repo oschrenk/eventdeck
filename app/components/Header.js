9ca14606267e51be43cccf55ad413bf724b6edff
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-12 15:30:35 +0100

    Extract Header component

---
+++ app/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  View}
+from 'react-native';
+
+import { useNavigation } from '@react-navigation/native'
+
+import { CardIcon } from '../components/Icon'
+import { partyName } from '../styles/typography'
+
+export default Header = ({title}) => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"}}>
+      <View style={{width: 32, marginLeft: 15, flexDirection: 'column', alignItems: "center"}}>
+        <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
+          <CardIcon name={'burger'} style={{fontSize: 26}}/>
+        </TouchableOpacity>
+      </View>
+      <Text style={{...partyName, textAlign: 'center'}}>{title}</Text>
+      <View style={{width: 32, marginRight: 15}} />
+    </View>
+  )
+}
+
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -3,21 +3,16 @@ import {
   ImageBackground,
   SafeAreaView,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View}
 from 'react-native';
 import CardFlip from 'react-native-card-flip'
 
 import Card from '../components/Card'
-import { CardIcon } from '../components/Icon'
-import { useCardDeck } from "../contexts/CardDeckContext"
+import Header from '../components/Header'
 import ImageButton from '../components/ImageButton'
-import { partyName } from '../styles/typography'
 import SwipeGesture from '../components/SwipeGesture'
-import { useNavigation } from '@react-navigation/native'
-import { DrawerActions } from '@react-navigation/native'
 
+import { useCardDeck } from "../contexts/CardDeckContext"
 
 const swipe = (card) => (action) => {
   switch(action){
@@ -35,19 +30,10 @@ const swipe = (card) => (action) => {
 const HomeScreen = () => {
   const { currentCard, currentParty, drawCard } = useCardDeck()
   const [cardRef, setCardRef] = useState(null)
-  const navigation = useNavigation();
   return (
     <ImageBackground source={require('../../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
       <SafeAreaView>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"}}>
-          <View style={{width: 32, marginLeft: 15, flexDirection: 'column', alignItems: "center"}}>
-            <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
-              <CardIcon name={'burger'} style={{fontSize: 26}}/>
-            </TouchableOpacity>
-          </View>
-          <Text style={{...partyName, textAlign: 'center'}}>{currentParty().name}</Text>
-          <View style={{width: 32, marginRight: 15}} />
-        </View>
+        <Header title={currentParty().name} />
         <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', height: 48}}>
           <ImageButton source={require('../../assets/images/city_button.png')} onPress={() => drawCard('city')} style={{ height: 40, width: 132}}/>
           <ImageButton source={require('../../assets/images/road_button.png')} onPress={() => drawCard('road')} style={{ height: 40, width: 132 }}/>
