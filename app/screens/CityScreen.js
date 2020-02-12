c6e4161e6df28e2ff73398ab9a35a2a44320ae49
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-12 16:22:00 +0100

    Prettier City events screen

--- app/screens/CityScreen.js
+++ app/screens/CityScreen.js
@@ -16,25 +16,6 @@ import Header from '../components/Header'
 import { useCardDeck, allCityCardNumbers } from "../contexts/CardDeckContext";
 import { frontText } from '../styles/typography'
 
-const Column = ({ids, checked, toggle}) => {
-  return (
-    <View style={{width: 80 }}>
-      <FlatList
-        data={ids}
-        renderItem={({ item }) =>
-          <Checkbox
-            label={item.toString()}
-            checked={checked(item)}
-            labelStyle={frontText}
-            onChange={(item) => toggle(item)}
-          />
-        }
-        keyExtractor={item => item.toString()}
-      />
-    </View>
-  )
-}
-
 const H1 = ({title}) => {
   return (
     <ImageBackground source={require('../../assets/images/h1.png')} style={{width: 188, height: 52}}>
@@ -43,7 +24,7 @@ const H1 = ({title}) => {
   )
 }
 
-const CityScreen = ({navigation}) => {
+const CityScreen = () => {
   const { isAvailable, toggleAvailable } = useCardDeck()
 
   return (
@@ -52,29 +33,41 @@ const CityScreen = ({navigation}) => {
         <ScrollView>
           <Header title={"City Events"} />
           <H1 title={"Base"} />
-          <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
-              <Column ids={allCityCardNumbers.slice(0,6)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(6,12)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(12,18)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(18,24)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(24,30)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+          <View style={{marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+          {
+            allCityCardNumbers.slice(0,30).map(e =>
+              <Checkbox
+                label={e.toString()}
+                checked={isAvailable(e, 'city')}
+                labelStyle={{...frontText, width: 14}}
+                onChange={(item) => toggleAvailable(item, 'city')}
+              />
+          )}
           </View>
           <H1 title={"Unlocks"} />
-          <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
-              <Column ids={allCityCardNumbers.slice(30,40)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(40,50)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(50,60)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(60,70)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(70,81)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+          <View style={{marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+          {
+            allCityCardNumbers.slice(30,81).map(e =>
+              <Checkbox
+                label={e.toString()}
+                checked={isAvailable(e, 'city')}
+                labelStyle={{...frontText, width: 14}}
+                onChange={(item) => toggleAvailable(item, 'city')}
+              />
+          )}
           </View>
 
           <H1 title={"Forgotten Circles"} />
-          <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
-              <Column ids={allCityCardNumbers.slice(81,82)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(82,84)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(84,86)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(86,88)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
-              <Column ids={allCityCardNumbers.slice(88,90)} checked={(item) => isAvailable(item, 'city')} toggle={(item) =>toggleAvailable(item, 'city')}/>
+          <View style={{marginLeft: 15, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+          {
+            allCityCardNumbers.slice(81,90).map(e =>
+              <Checkbox
+                label={e.toString()}
+                checked={isAvailable(e, 'city')}
+                labelStyle={{...frontText, width: 14}}
+                onChange={(item) => toggleAvailable(item, 'city')}
+              />
+          )}
           </View>
         </ScrollView>
       </SafeAreaView>
