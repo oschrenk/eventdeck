d00ec6fe544150c938f1d9abbd1be9874b8ac2ff
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-13 15:27:35 +0100

    Prettify History screen

--- app/screens/HistoryScreen.js
+++ app/screens/HistoryScreen.js
@@ -1,116 +1,113 @@
 import React from "react";
-import { FlatList, ImageBackground, SafeAreaView, Text, View } from 'react-native';
+import {
+  FlatList,
+  SectionList,
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  View
+} from 'react-native';
 
-import { header } from '../styles/typography'
+import formatISO from 'date-fns/formatISO'
 
 import { useCardDeck } from "../contexts/CardDeckContext"
+import Header from '../components/Header'
+import H1 from '../components/H1'
 import { CardIcon } from '../components/Icon'
 import EnrichedText from '../components/EnrichedText'
 
-import * as RNLocalize from "react-native-localize"
-
-const capitalize = (s) => {
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-
 const formatDate = (t) => {
   const date = new Date(t)
-  const options = {
-    dateStyle: 'short',
-    timeStyle: 'short',
-    timeZone: RNLocalize.getTimeZone(),
-  }
-  return new Intl.DateTimeFormat('de-DE', options).format(date)
-}
-
-const CardDrawn = ({event}) => {
-  const {timestamp, data} = event
-  const {id, type} = data
-  return (
-    <View style={{flexDirection: "row"}}>
-      <View style={{width: 32, height: 32}}>
-        <CardIcon name={'draw'} style={{fontSize: 32, color: "black" }} key={'draw'}/>
-      </View>
-      <View>
-        <Text>{`${capitalize(type)} ${id} at ${formatDate(timestamp)}`}</Text>
-      </View>
-    </View>
-  )
+  return formatISO(date, { representation: 'date' })
 }
 
 const Effect = ({text}) => {
   return (
     <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-      <EnrichedText text={text}/>
+      <EnrichedText text={text} style={{fontFamily: 'Crimson-Bold', fontSize: 16}}/>
     </View>
   )
 }
 
-const CardRemoved = ({event}) => {
-  const {timestamp, data} = event
-  const {id, type} = data
-  return (
-    <View style={{flexDirection: "row"}}>
-      <View style={{width: 32, height: 32, alignItems: "center" }}>
-        <CardIcon name={'remove-from-game'} style={{fontSize: 26, color: "black" }} />
-      </View>
-      <View>
-        <Text>{`${capitalize(type)} ${id} at ${formatDate(timestamp)}`}</Text>
-        { data.effects &&
-          data.effects.map((e,i) =>
-            <Effect text={e} key={i}/>
-          )
-        }
-      </View>
-    </View>
-  )
-}
-
-const CardReturned = ({event}) => {
-  const {timestamp, data} = event
-  const {id, type} = data
-  return (
-    <View style={{flexDirection: "row"}}>
-      <View style={{width: 32, height: 32, alignItems: "center" }}>
-        <CardIcon name={'return-to-deck'} style={{fontSize: 26, color: "black" }} />
-      </View>
-      <View>
-        <Text>{`${capitalize(type)} ${id} at ${formatDate(timestamp)}`}</Text>
-        { data.effects &&
-          data.effects.map((e,i) =>
-            <Effect text={e} key={i}/>
-          )
-        }
-      </View>
-    </View>
-  )
-}
-
-const Item = (item) => {
-
-  const {name, timestamp} = item.item
-
-  switch (name) {
-    case 'CardDrawn':
-      return (<CardDrawn event={item.item}/>)
-    case 'CardRemoved':
-      return (<CardRemoved event={item.item}/>)
-    case 'CardReturned':
-      return (<CardReturned event={item.item}/>)
+const resolveIcon = (eventName) => {
+  if (eventName === 'CardReturned') {
+    return (<CardIcon name={'return-to-deck'} style={{fontSize: 26, color: "black" }} /> )
+  } else if (eventName === 'CardRemoved') {
+    return (<CardIcon name={'remove-from-game'} style={{fontSize: 26, color: "black" }} /> )
   }
+  return null
+}
 
+const Event = ({value}) => {
+  const {name, data} = value
+  const {id, type, option, effects} = data
+  const cardIcon = (type === 'city') ? require('../../assets/images/city-icon.png') : require('../../assets/images/road-icon.png')
+  const color = (type === 'city') ? 'white' : 'black'
+  return (
+    <View style={{flexDirection: "row", marginLeft: 15, marginRight: 15, marginBottom: 10}}>
+      <View>
+        <ImageBackground source={cardIcon} style={{width: 34, height: 48, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{textAlign: 'center', fontFamily: 'Pirata One', fontSize: 18,lineHeight: 20, color: color, marginTop: 8}}>{`${id}\n\n${option.slice(-1)}`}</Text>
+        </ImageBackground>
+      </View>
+      <View style={{marginLeft: 5}}>
+      { effects &&
+        effects.map((e,i) =>
+          <Effect text={e} key={i}/>
+        )
+      }
+      </View>
+      <View>
+      {
+        resolveIcon(name)
+      }
+      </View>
+    </View>
+
+  )
+}
+
+const groupBy = (list, keyGetter) => {
+    const map = new Map();
+    list.forEach((item) => {
+         const key = keyGetter(item);
+         const collection = map.get(key);
+         if (!collection) {
+             map.set(key, [item]);
+         } else {
+             collection.push(item);
+         }
+    });
+    return map;
+}
+const groupedEvents = (history) => {
+  const data = history.filter(e => (e.name === 'CardRemoved') || (e.name === 'CardReturned'))
+  const grouped = groupBy(data, event => formatDate(event.timestamp))
+  const iter = grouped.entries()
+  var entries = []
+  let result = iter.next();
+  while (!result.done) {
+    const [k,v] = result.value
+    entries.push({title: k, data: v})
+    result = iter.next();
+  }
+  return entries
 }
 
 const HistoryScreen = () => {
   const { currentParty } = useCardDeck()
+  const data = groupedEvents(currentParty().history)
   return (
     <ImageBackground source={require('../../assets/images/background.jpg')} style={{width: '100%', height: '100%'}}>
       <SafeAreaView>
-        <Text style={header}>History</Text>
-        <FlatList
-         data={currentParty().history.filter(e => (e.name === 'CardRemoved') || (e.name === 'CardReturned'))}
-         renderItem={({item}) => <Item item={item}></Item>}
-         keyExtractor={item => item.timestamp.toString() }
+        <Header title={"History"} />
+        <SectionList
+          sections={data}
+          keyExtractor={item => item.timestamp}
+          renderItem={({ item }) => <Event value={item} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <H1 title={title} />
+          )}
         />
       </SafeAreaView>
     </ImageBackground>
@@ -118,5 +115,3 @@ const HistoryScreen = () => {
 };
 
 export default HistoryScreen;
-
-
